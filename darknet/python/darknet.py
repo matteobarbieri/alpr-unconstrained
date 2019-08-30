ee6864c071f9603eb19792b6c46c94250bcd36b6
Matteo Barbieri <neopard@gmail.com>
2019-08-30 13:38:06 +0200

    Update darknet

--- darknet/python/darknet.py
+++ darknet/python/darknet.py
@@ -2,6 +2,8 @@ from ctypes import *
 import math
 import random
 
+import sys
+
 def sample(probs):
     s = sum(probs)
     probs = [a/s for a in probs]
@@ -42,7 +44,7 @@ class METADATA(Structure):
     _fields_ = [("classes", c_int),
                 ("names", POINTER(c_char_p))]
 
-    
+
 
 #lib = CDLL("/home/pjreddie/documents/darknet/libdarknet.so", RTLD_GLOBAL)
 lib = CDLL("darknet/libdarknet.so", RTLD_GLOBAL)
@@ -123,6 +125,7 @@ def classify(net, meta, im):
     return res
 
 def detect(net, meta, image, thresh=.5, hier_thresh=.5, nms=.45):
+    print("here!")
     im = load_image(image, 0, 0)
     num = c_int(0)
     pnum = pointer(num)
@@ -142,16 +145,23 @@ def detect(net, meta, image, thresh=.5, hier_thresh=.5, nms=.45):
     free_image(im)
     free_detections(dets, num)
     return res,wh
-    
+
 if __name__ == "__main__":
     #net = load_net("cfg/densenet201.cfg", "/home/pjreddie/trained/densenet201.weights", 0)
     #im = load_image("data/wolf.jpg", 0, 0)
     #meta = load_meta("cfg/imagenet1k.data")
     #r = classify(net, meta, im)
     #print r[:10]
-    net = load_net("cfg/tiny-yolo.cfg", "tiny-yolo.weights", 0)
-    meta = load_meta("cfg/coco.data")
-    r = detect(net, meta, "data/dog.jpg")
-    print r
-    
+
+    # bytes(img_path, encoding='utf-8')
+
+    # net = load_net("cfg/tiny-yolo.cfg", "tiny-yolo.weights", 0)
+    # meta = load_meta("cfg/coco.data")
+    # r = detect(net, meta, "data/dog.jpg")
+
+    net = load_net(b"data/vehicle-detector/yolo-voc.cfg", b"data/vehicle-detector/yolo-voc.weights", 0)
+    meta = load_meta(b"data/vehicle-detector/voc.data")
+    r = detect(net, meta, b"samples/test/03009.jpg")
+    print( r)
+
 
