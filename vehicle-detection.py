624839e4e1cf81c2ddb0c5f0915b9bc7b8ae6fff
Matteo Barbieri <neopard@gmail.com>
2019-09-11 11:32:11 +0200

    Code refactoring in main script

--- vehicle-detection.py
+++ vehicle-detection.py
@@ -5,10 +5,10 @@ import traceback
 
 import darknet.python.darknet as dn
 
-from src.label              import Label, lwrite
-from os.path                import splitext, basename, isdir
-from os                     import makedirs
-from src.utils              import crop_region, image_files_from_folder
+from src.label import Label, lwrite
+from os.path import splitext, basename, isdir
+from os import makedirs
+from src.utils import crop_region, image_files_from_folder
 from darknet.python.darknet import detect
 
 
@@ -16,16 +16,20 @@ if __name__ == '__main__':
 
     try:
 
-        input_dir  = sys.argv[1]
+        input_dir = sys.argv[1]
         output_dir = sys.argv[2]
 
         vehicle_threshold = .5
 
-        vehicle_weights = b'data/vehicle-detector/yolo-voc.weights'
-        vehicle_netcfg  = b'data/vehicle-detector/yolo-voc.cfg'
-        vehicle_dataset = b'data/vehicle-detector/voc.data'
+        # vehicle_weights = b'data/vehicle-detector/yolo-voc.weights'
+        # vehicle_netcfg  = b'data/vehicle-detector/yolo-voc.cfg'
+        # vehicle_dataset = b'data/vehicle-detector/voc.data'
 
-        vehicle_net  = dn.load_net(vehicle_netcfg, vehicle_weights, 0)
+        vehicle_weights = b'darknet/weights/yolov3.weights'
+        vehicle_netcfg = b'darknet/cfg/yolov3.cfg'
+        vehicle_dataset = b'data/vehicle-detector/coco.data'
+
+        vehicle_net = dn.load_net(vehicle_netcfg, vehicle_weights, 0)
         vehicle_meta = dn.load_meta(vehicle_dataset)
 
         imgs_paths = image_files_from_folder(input_dir)
@@ -36,45 +40,46 @@ if __name__ == '__main__':
 
         print('Searching for vehicles using YOLO...')
 
-        for i,img_path in enumerate(imgs_paths):
+        for i, img_path in enumerate(imgs_paths):
 
-            print( '\tScanning %s' % img_path)
+            print('\tScanning %s' % img_path)
 
             bname = basename(splitext(img_path)[0])
 
-            # R,_ = detect(vehicle_net, vehicle_meta, img_path ,thresh=vehicle_threshold)
-            # R,_ = detect(vehicle_net, vehicle_meta, bytes(img_path, encoding='utf-8') ,thresh=vehicle_threshold)
-            R, _ = detect(vehicle_net, vehicle_meta, b"samples/test/03009.jpg")
+            R, _ = detect(
+                vehicle_net, vehicle_meta,
+                bytes(img_path, encoding='utf-8'),
+                thresh=vehicle_threshold)
 
-            # print(R)
+            # R = [r for r in R if r[0] in [b'car', b'bus']]
+            R = [r for r in R if r[0] in [b'car', b'bus', b'truck']]
 
-            R = [r for r in R if r[0] in [b'car', b'bus']]
-
-            print( '\t\t%d cars found' % len(R))
+            print('\t\t%d cars found' % len(R))
 
             if len(R):
 
                 Iorig = cv2.imread(img_path)
-                WH = np.array(Iorig.shape[1::-1],dtype=float)
+                WH = np.array(Iorig.shape[1::-1], dtype=float)
                 Lcars = []
 
-                for i,r in enumerate(R):
+                for i, r in enumerate(R):
 
-                    cx,cy,w,h = (np.array(r[2])/np.concatenate( (WH,WH) )).tolist()
+                    cx, cy, w, h = (
+                        np.array(r[2])/np.concatenate((WH, WH))).tolist()
                     tl = np.array([cx - w/2., cy - h/2.])
                     br = np.array([cx + w/2., cy + h/2.])
-                    label = Label(0,tl,br)
-                    Icar = crop_region(Iorig,label)
+                    label = Label(0, tl, br)
+                    Icar = crop_region(Iorig, label)
 
                     Lcars.append(label)
 
-                    cv2.imwrite('%s/%s_%dcar.png' % (output_dir,bname,i),Icar)
+                    cv2.imwrite(
+                        '%s/%s_%dcar.png' % (output_dir, bname, i), Icar)
 
-                lwrite('%s/%s_cars.txt' % (output_dir,bname),Lcars)
+                lwrite('%s/%s_cars.txt' % (output_dir, bname), Lcars)
 
     except:
         traceback.print_exc()
         sys.exit(1)
 
     sys.exit(0)
-
