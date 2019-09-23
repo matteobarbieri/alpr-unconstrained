f186b828e9da1a6ebc5352faf86d8e8fba46007b
Matteo Barbieri <neopard@gmail.com>
2019-09-23 16:58:56 +0200

    Move utility functions in dedicated module
    Code refactoring

--- post-process-detections.py
+++ post-process-detections.py
@@ -6,7 +6,8 @@ import json
 
 import numpy as np
 
-from utils import guess_last_frame
+from utils import (guess_last_frame, area,
+                   iterative_levenshtein, compute_center_coordinates)
 
 
 def parse_args():
@@ -47,43 +48,6 @@ def parse_args():
     return args
 
 
-def iterative_levenshtein(s, t):
-    """
-        iterative_levenshtein(s, t) -> ldist
-        ldist is the Levenshtein distance between the strings
-        s and t.
-        For all i and j, dist[i,j] will contain the Levenshtein
-        distance between the first i characters of s and the
-        first j characters of t
-    """
-    rows = len(s)+1
-    cols = len(t)+1
-    dist = [[0 for x in range(cols)] for x in range(rows)]
-    # source prefixes can be transformed into empty strings
-    # by deletions:
-    for i in range(1, rows):
-        dist[i][0] = i
-    # target prefixes can be created from an empty source string
-    # by inserting the characters
-    for i in range(1, cols):
-        dist[0][i] = i
-
-    for col in range(1, cols):
-        for row in range(1, rows):
-            if s[row-1] == t[col-1]:
-                cost = 0
-            else:
-                cost = 1
-            dist[row][col] = min(dist[row-1][col] + 1,      # deletion
-                                 dist[row][col-1] + 1,      # insertion
-                                 dist[row-1][col-1] + cost) # substitution
-    # for r in range(rows):
-        # print(dist[r])
-
-
-    return dist[row][col]
-
-
 def most_similar_plate(target_plate, plate_list, max_distance=2):
     """
     Given a target license plate and a list of candidate plates, find the one
@@ -111,7 +75,7 @@ def most_similar_plate(target_plate, plate_list, max_distance=2):
             if ld <= max_distance and ld < min_distance:
                 min_distance = ld
                 mspi = i
-    except:
+    except:  # noqa
         pass
 
     return mspi
@@ -189,10 +153,10 @@ def recover_missing_plates(annotations_history, i, plates, window,  # noqa
             plates_fixed += 1
             continue
 
-        # if (
-                # plate_code not in current_plate_codes and
-                # len(plates_past[plate_code]['bb']) >= min_occurrences and
-                # len(plates_future[plate_code]['bb']) >= min_occurrences):
+        # if ( # noqa
+                # plate_code not in current_plate_codes and # noqa
+                # len(plates_past[plate_code]['bb']) >= min_occurrences and # noqa
+                # len(plates_future[plate_code]['bb']) >= min_occurrences): # noqa
         if (
                 len(plates_past[plate_code]['bb']) >= min_occurrences and
                 len(plates_future[plate_code]['bb']) >= min_occurrences):
@@ -235,19 +199,6 @@ def recover_missing_plates(annotations_history, i, plates, window,  # noqa
     print("Fixed {} plates".format(plates_fixed))
 
     return plates
-
-
-def compute_center_coordinates(bb):
-    """
-    Computes the coordinates of the center of a rectangular region
-
-    bb = [x, y, w, h]
-    """
-
-    cx = bb[0] + bb[2]/2
-    cy = bb[1] + bb[3]/2
-
-    return cx, cy
 
 
 def associate_plates_to_car(cars, plates):
@@ -376,21 +327,6 @@ def process_annotations(annotations_history, i, window=25):
     cars = associate_plates_to_car(cars, plates_unique)
 
     return {'cars': cars}
-
-
-def area(a, b):  # returns None if rectangles don't intersect
-
-    ax, ay, aw, ah = a
-    bx, by, bw, bh = b
-
-    # dx = min(a.xmax, b.xmax) - max(a.xmin, b.xmin)
-    # dy = min(a.ymax, b.ymax) - max(a.ymin, b.ymin)
-
-    dx = min(ax + aw, bx + bw) - max(ax, bx)
-    dy = min(ay + ah, by + bh) - max(ay, by)
-
-    if dx >= 0 and dy >= 0:
-        return dx * dy
 
 
 def main():
